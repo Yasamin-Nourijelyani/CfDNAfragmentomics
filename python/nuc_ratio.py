a77163129d0db3c67b31c54a2bafd9d5deb3ea42
Yasamin Nouri Jelyani <yasamin.nourijelyani@mail.utoronto.ca>
2023-04-02 23:34:29 -0400

    fixed error

--- python/nuc_ratio.py
+++ python/nuc_ratio.py
@@ -16,25 +16,29 @@ def nucleosome_ratio(controls_bed, sample_bed):
     """Check the difference of cfDNA fragment length between sample and control data, by checking the mono-nucleosome and di-nucleosome fragment length 
     differences uses Wilcoxon rank sum test.
     
-    Sample use:
-      
-      python ../python/nuc_ratio.py --sample lung_cancer_data/EE88183.hg38.frag.bed healthy_data/EE86217.hg38.frag.bed
-    
     """
-    # all of the length data
-    lengths_control = []
+    # mononucleosome length data
+    lengths_control_mono = []
+
+    lengths_control_di = []
+
 
     with open(controls_bed, 'r') as c_bed:
         reader = csv.reader(c_bed, delimiter='\t')
-        print("Getting sample lengths")
+        print("Getting control lengths")
         for row in tqdm(reader):
             start = int(row[1])
             end = int(row[2])
             length = end - start
-            lengths_control.append(length)
+            if length < 150:
+                lengths_control_mono.append(length)
+            elif length >= 275 & length <= 400:
+                lengths_control_di.append(length) 
 
     
-    lengths_sample = []
+    lengths_sample_mono = []
+    lengths_sample_di = []
+
     with open(sample_bed, 'r') as s_bed:
         reader = csv.reader(s_bed, delimiter='\t')
         print("Getting sample lengths")
@@ -42,21 +46,19 @@ def nucleosome_ratio(controls_bed, sample_bed):
             start = int(row[1])
             end = int(row[2])
             length = end - start
-            lengths_sample.append(length)
+            if length < 150:
+                lengths_sample_mono.append(length)
+            elif length >= 275 & length <= 400:
+                lengths_sample_di.append(length)
 
 
 
-    mono_nuc_control = lengths_control[lengths_control < 150]
-    mono_nuc_sample = lengths_sample[lengths_sample < 150]
 
-    statistic_mono, pvalue_mono = stats.ranksums(mono_nuc_control, mono_nuc_sample)
+    statistic_mono, pvalue_mono = stats.ranksums(lengths_control_mono, lengths_sample_mono)
 
 
-    di_nuc_control = lengths_control[(lengths_control >= 275) & (lengths_control <= 400)]
-    
-    di_nuc_sample = lengths_sample[(lengths_sample >= 275) & (lengths_sample <= 400)]
 
-    statistic_di, pvalue_di = stats.ranksums(di_nuc_control, di_nuc_sample)
+    statistic_di, pvalue_di = stats.ranksums(lengths_control_di, lengths_sample_di)
 
 
 
